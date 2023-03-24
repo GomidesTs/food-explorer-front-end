5f9c41c4ddd951e943ef452ab3f57d6712068286
GomidesTs <gomidestulio5@gmail.com>
2023-03-24 15:27:01 -0300

    :lipstick: feat: added Button component styling
    This update adds the Button component styling to the project. The Button component was created earlier and now has an attractive and modern look.

---
+++ src/components/Button/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 4.8rem;
+    padding: 1.2rem 3.3rem;
+    gap: 0.8rem;
+    border: 0;
+    border-radius: 0.5rem;
+    
+    font-weight: 500;
+    font-size: 1.4rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_200};;
+
+    background-color: ${({ theme }) => theme.COLORS.TOMATO_400};
+`
