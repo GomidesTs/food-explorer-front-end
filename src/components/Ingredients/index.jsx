ce76cb6d4ec08b2de2dcb8079880df46e9de5f26
GomidesTs <gomidestulio5@gmail.com>
2023-03-27 19:48:10 -0300

    :sparkles: feat: Ingredient component inclusion
    This update adds the Ingredients component. This display will be responsible for showing which ingredients a proto has in its composition.

---
+++ src/components/Ingredients/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export function Ingredients({ title, ...rest }) {
+    return (
+        <Container {...rest}>
+            <span>{title}</span>
+        </Container>
+    )
+}
