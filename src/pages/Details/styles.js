a41aff2eeafeda20afdfb17e953f9b42243f31a8
GomidesTs <gomidestulio5@gmail.com>
2023-03-27 21:27:58 -0300

    :lipstick: feat: detail page styling
    This update adds styles to the detail page so that it displays information as designed in the project designer, colors used follow the consistent color palette of the project.

---
+++ src/pages/Details/styles.js
@@ -0,0 +1,95 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    max-width:100%;
+    height: 100vh;
+
+    display: grid;
+    grid-template-rows: 10rem auto;
+    grid-template-areas: 
+    'header'
+    'content';
+`
+
+export const Content = styled.main`
+    grid-area: content;
+
+    overflow-y: auto;
+
+    padding: 6.4rem 3rem;
+`
+
+export const Dish = styled.section`
+    max-width: 121rem;
+    margin: 0 auto;
+
+    .description{
+        display: flex;
+        flex-direction: column;
+
+        align-items: center;
+        
+        width: 100%;
+        max-width: 121.2rem;
+        margin: auto;
+        padding: 3.5rem 4rem;
+
+        h1 {
+            font-size: clamp(2rem, 4vw, 4rem);
+            font-weight: 500;
+
+            padding-bottom: 2.4rem;
+        }
+
+        p {
+            max-width: 60rem;
+
+            font-size: 1.8rem;
+            font-weight: 400;
+            line-height: 140%;
+
+            padding-bottom: 2.6rem;
+        }
+
+        img {
+                width: 100%;
+
+                border-radius: 50%;
+
+                object-fit: cover;
+            }
+        
+
+        @media only screen and (min-width: 768px) {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            gap: 2.4rem;
+
+            img {
+                width: 40rem;
+                height: 40rem;
+
+                border-radius: 50%;
+
+                object-fit: cover;
+            }
+        }
+    }
+`
+
+export const Action = styled.section`
+    display: flex;
+    align-items: center;
+    gap: 3.2rem;
+    
+
+    padding-top: 4.8rem;
+
+    > div {
+        display: flex;
+        align-items: center;
+        gap: 1.4rem;
+    }
+`
