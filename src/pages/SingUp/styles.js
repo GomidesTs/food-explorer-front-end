5d147109e6158660d118cf2bdd43aa2d5e82fca2
GomidesTs <gomidestulio5@gmail.com>
2023-03-27 21:40:33 -0300

    :lipstick: feat: added styling to login and signup pages
    This update adds styles to the Sign In and Sign Up pages responsible for user registration and login, both share the same designer modifying the entries that save and retrieve information.

---
+++ src/pages/SingUp/styles.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    max-width: 110.6rem;
+    height: 100vh;
+
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 6.4rem;
+
+    padding: 4rem;
+
+    > img {
+        width: 100%;
+    }
+
+    @media only screen and (min-width: 768px) {
+        flex-direction: row;
+    }
+`
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: left;
+
+    max-width: 47.6rem;
+    width: 100%;
+    
+    padding: 2rem;
+    border-radius: 1.6rem;
+
+    background-color: ${({ theme }) => theme.COLORS.DARK_400};
+    
+    fieldset {
+        border: none;
+        margin-bottom: 3.2rem;
+
+        legend {
+            border: none;
+            text-align: center;
+        }
+
+        .information {
+            label {
+                display: inline-block;
+                padding-bottom: .8rem;
+            }
+        }
+    }
+
+    a {
+        text-align: center;
+        color: ${({ theme }) => theme.COLORS.LIGHT_200}
+    }
+
+    @media only screen and (min-width: 800px) {
+        padding: 6.4rem;
+    }
+`
+
