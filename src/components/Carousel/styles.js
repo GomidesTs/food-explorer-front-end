8d791ced6012cd54e69e901afd7ad16943a064e1
GomidesTs <gomidestulio5@gmail.com>
2023-03-27 22:24:58 -0300

    :lipstick: feat: insert spacing and background
    In this update, the slider component suffers internal spacing modifications to suit the Card component layer that is inserted on the home page, the background colors were also inserted throughout the application so that they follow the project's color palette.

--- src/components/Carousel/styles.js
+++ src/components/Carousel/styles.js
@@ -1,30 +1,46 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    margin: 0 auto;
+    
     .swiper-slide {
-        padding: 6.4rem 0;
-
+        padding: 2rem 4rem;
         display: flex;
         justify-content: center;
         align-items: center;
-
+        
         border-radius: 2rem;
+        
+        background-color: ${({ theme }) => theme.COLORS.DARK_800};
+    }
+     
+    .swiper-button-prev {
+        background: linear-gradient(270deg, rgba(255,255,255,0) 0%, ${({theme}) => theme.COLORS.DARK_1000});
+    }
+
+    .swiper-button-next { 
+        background: linear-gradient(90deg, rgba(255,255,255,0) 0%, ${({theme}) => theme.COLORS.DARK_1000});
     }
 
     .swiper-button-next,
     .swiper-button-prev {
-        width: 9rem;
-        height: 51.2rem;
-        margin: -25.6rem -1rem;
+        width: 3rem;
+        height: 70rem;
         
         color: ${({ theme }) => theme.COLORS.CAKE_200};
         font-weight: bolder;
         mask-image: none;
 
         transition: .5s ease;
+
+        margin: -35.6rem -1rem;
         
         &:hover {
             color: ${({ theme }) => theme.COLORS.CAKE_100};
         }
+    }
+
+    .swiper-pagination-bullet {
+        background-color: ${({ theme }) => theme.COLORS.CAKE_100};
     }
 `
