7c246d7732ec4acff955c1fd3545bdefad9e41e1
GomidesTs <gomidestulio5@gmail.com>
2023-03-29 22:24:32 -0300

    :sparkles: feat: addition of the component new ingredients
    This update adds new ingredient components so that they become components as tags when rendering on other pages.

---
+++ src/components/NewIngredients/index.jsx
@@ -0,0 +1,25 @@
+import { Container } from './styles';
+
+import { RiAddFill, RiSubtractFill } from 'react-icons/ri';
+
+export function NewIngredients({ isNew, value, onClick, ...rest }) {
+    return (
+        <Container isNew={isNew}>
+            <input
+                type='text'
+                value={value}
+                readOnly={!isNew}
+                {...rest}
+                list='ingredientName'
+            />
+
+            <button
+                type='button'
+                onClick={onClick}
+                className={isNew ? 'button-add' : 'button-delete'}
+            >
+                {isNew ? <RiAddFill /> : <RiSubtractFill />}
+            </button>
+        </Container>
+    )
+}
