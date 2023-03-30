3a161e01373d971d59c34cefeafd0a4c9d9b3ee4
GomidesTs <gomidestulio5@gmail.com>
2023-03-29 22:28:11 -0300

    :lipstick: feat: new ingredient component styling
    This update adds new ingredient component styling, so it will have the colors as the project designer when rendering it will behave like tags in the canvas show.

---
+++ src/components/NewIngredients/styles.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    
+    padding: 0 1.6rem;
+    border-radius: 0.8rem;
+    border: ${({ theme, isNew }) => isNew ? `.1rem dashed ${theme.COLORS.LIGHT_500}` : 'none'};
+
+    color: ${({ theme }) => theme.COLORS.LIGHT_500};
+    background-color: ${({ theme, isNew }) => isNew ? 'transparent' : theme.COLORS.DARK_200};
+    
+    svg {
+        vertical-align: middle;
+    }
+
+    > button {
+        border: none;
+        background: none;
+    }
+
+    .button-delete {
+        color: ${({ theme }) => theme.COLORS.TOMATO_300};
+    }
+
+    .button-add {
+        color: ${({ theme }) => theme.COLORS.CAKE_100};
+    }
+
+    > input {
+        max-width: 13rem;
+        height: 2.8rem;
+
+        border: none;
+        
+        color: ${({ theme }) => theme.COLORS.LIGHT_200};
+        background: transparent;
+
+        &::placeholder {
+            color: ${({ theme }) => theme.COLORS.LIGHT_500};
+        }
+    }
+`;
