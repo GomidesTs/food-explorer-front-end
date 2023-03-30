028398f4203cd41b5243b6d035f3a4c139bc9137
GomidesTs <gomidestulio5@gmail.com>
2023-03-29 22:21:18 -0300

    :lipstick: feat: added product creation styling
    This update adds styling to the dish creation page, on mobile devices the information roads are one below the other, on desktop they will be in grid display.

---
+++ src/pages/CreateDish/styles.js
@@ -0,0 +1,174 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    max-width:100%;
+    height: 100vh;
+
+    display: grid;
+    grid-template-rows: 10rem auto;
+    grid-template-areas: 
+    'header'
+    'content';
+`
+
+export const Content = styled.main`
+    grid-area: content;
+
+    overflow-y: auto;
+
+    padding: 6.4rem 3rem;
+    margin: 0 auto;
+
+    max-width: 120rem;
+`
+
+export const Form = styled.form`
+    padding-top: 3rem;
+    width: 100%;
+
+    fieldset {
+        display: flex;
+        flex-direction: column;
+        grid-template-columns: 1fr 2fr 1fr;
+        gap: 2rem;
+
+        border: none;
+
+        label {
+            display: block;
+            margin-bottom: .8rem;
+        }
+        
+        .dishImage {
+            input[type='file'] {
+                display: none;
+            }
+
+            > div {
+                label {
+                    margin-top: .8rem;
+                    width: 100%;
+                    height: 4.8rem;
+
+                    padding: 1.6rem 1.4rem;
+
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+
+                    background-color: ${({ theme }) => theme.COLORS.DARK_200};
+
+                    border: none;
+                    border-radius: .8rem;
+
+                    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+                }
+            }
+        }
+
+       
+        .dishCategory {
+            display: flex;
+            flex-direction: column;
+
+            select {
+                width: 100%;
+                height: 4.8rem;
+
+                padding: 1.6rem 1.4rem;
+
+                display: flex;
+                align-items: center;
+                justify-content: center;
+
+                background-color: ${({ theme }) => theme.COLORS.DARK_200};
+
+                border: none;
+                border-radius: .8rem;
+
+                color: ${({ theme }) => theme.COLORS.LIGHT_400};
+
+                &:focus {
+                    outline: none;
+                }
+            }
+        }
+
+        .newIngredients {
+            display: flex;
+            flex-direction: column;
+
+            .ingredients {
+                display: flex;
+                flex-wrap: wrap;
+                align-content: center;
+                
+                width: 100%;
+                height: auto;
+                padding: 0.8rem;
+                margin-bottom: 0;
+
+                border-radius: 0.8rem;
+                border: .1rem solid ${({ theme }) => theme.COLORS.DARK_100};
+            }
+        }
+
+        .price {
+            input[type=number]::-webkit-outer-spin-button,
+            input[type=number]::-webkit-inner-spin-button {
+                -webkit-appearance: none;
+                margin: 0;
+            }
+
+            input[type=number] {
+            -moz-appearance: textfield;
+            }
+        }
+
+        .description {
+            display: flex;
+            flex-direction: column;
+            
+            textarea {
+                height: 20rem;
+
+                resize: none;
+
+                background-color: ${({ theme }) => theme.COLORS.DARK_200};
+
+                color: ${({ theme }) => theme.COLORS.LIGHT_400};
+
+                border: none;
+                border-radius: 0.8rem;
+
+                padding: 1.6rem 1.4rem;
+            }
+        }
+    }
+
+    @media only screen and (min-width: 768px) {
+        fieldset {
+            display: grid;
+            grid-template-columns: 1fr 2fr 1fr;
+
+            .dishName {
+                grid-column-start: 2;
+                grid-column-end: 3;
+            }
+
+            .newIngredients {
+                grid-column-start: 1;
+                grid-column-end: 3;              
+            }
+            
+            .description {
+                grid-column-start: 1;
+                grid-column-end: 4;
+            }
+
+            .button {
+                grid-column: 3;
+            }
+        }
+    }
+`
