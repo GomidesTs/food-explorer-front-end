999022069bc0fecfd2f302034bdd3942aba148a1
GomidesTs <gomidestulio5@gmail.com>
2023-04-03 20:50:19 -0300

    :sparkles: feat: user registration
    This update carried out several implementations in the system, with emphasis on the connection with the API, which allowed the integration of the system with other platforms.
    
    In addition, the handleSignUp function was developed, whose purpose is to create a new user. It is worth mentioning that, before allowing the creation, the function checks if all the necessary information has been provided by the user and if the password has at least 6 characters. Otherwise, the user is not created.
    
    The handleKeyDown function was also created, being activated when pressing the Enter key after entering the password. When triggered, the function calls the handleSignUp function to create the new user.
    
    To improve the user experience, a Link component was included, which allows navigation between the different pages of the system in an intuitive and simple way.

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,18 +1,64 @@
-import { Container, Form } from './styles';
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { Container, Form } from './styles'
 
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 
+import { api } from '../../services/api'
+
 import LogoFoodExplore from '../../assets/LogoFoodExplore.svg'
 
 export function SignUp() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const navigate = useNavigate()
+
+    function handleSignUp() {
+        if (!name || !email || !password) {
+            return alert('Preencha todos os campos!')
+        }
+
+        if (password.length < 6) {
+            alert('A senha deve conter pelo menos 6 caracteres')
+            return
+        }
+
+        setLoading(true)
+
+        api.post('/users', { name, email, password })
+            .then(() => {
+                alert('Usuário cadastrado com sucesso!')
+                navigate(-1)
+                setLoading(false)
+            })
+            .catch(error => {
+                if (error.response) {
+                    alert(error.response.data.message)
+                } else {
+                    alert('Não foi possível cadastrar')
+                }
+
+                setLoading(false)
+            })
+    }
+
+    function handleKeyDown(e) {
+        if (e.key === 'Enter') {
+            handleSignUp()
+        }
+    }
+
     return (
         <Container>
             <img src={LogoFoodExplore} alt='Logo do restaurante fictício Food Explore, este logo tem um losango na cor azul-escuro e ao lago escrito Food Explore na cor branca.' />
 
             <Form>
                 <fieldset>
-
                     <legend>Crie sua conta</legend>
 
                     <div className='information'>
@@ -22,6 +68,7 @@ export function SignUp() {
                             id='name'
                             type='text'
                             placeholder='Exemplo: Tulio Gomides'
+                            onChange={e => setName(e.target.value)}
                         />
                     </div>
 
@@ -32,6 +79,7 @@ export function SignUp() {
                             id='email'
                             type='text'
                             placeholder='exemplo@exemplo.com.br'
+                            onChange={e => setEmail(e.target.value)}
                         />
                     </div>
 
@@ -42,17 +90,21 @@ export function SignUp() {
                             id='password'
                             type='text'
                             placeholder='No mínimo 6 caracteres'
+                            onChange={e => setPassword(e.target.value)}
+                            onKeyDown={handleKeyDown}
                         />
                     </div>
 
                     <Button
-                        title='Criar conta'
+                        title={loading ? 'Salvando dados' : 'Criar conta'}
+                        onClick={handleSignUp}
+                        disabled={loading}
                     />
                 </fieldset>
 
-                <a href="#">
-                Já tenho uma conta
-                </a>
+                <Link to='/'>
+                    Já tenho uma conta
+                </Link>
             </Form>
         </Container>
     )
