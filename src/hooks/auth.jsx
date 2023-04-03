8c8ea18d36566749b12ef3982242359f3c5ce55b
GomidesTs <gomidestulio5@gmail.com>
2023-04-03 19:17:45 -0300

    :lock: feat: added the Auth
    This update adds oAuth being responsible for setting an authentication context using createContext, used to store user authentication information.
    
    Through functions, it performs several tasks, such as:
    
    The signIn function is used to authenticate the user, it sends a POST request to the API to login with the user's email and password. If the credentials are valid, the user's token is stored in the browser's local storage and added to the authorization headers in all future requests to the API.
    
    The signOut function removes user information from the browser's local storage and component state.
    
    The signOut function removes user information from the browser's local storage and component state.
    
    The updateProfile function is used to update the user's profile. If the user uploads an avatar file, it is sent to the API to be saved. User information is updated in the browser's API and local storage.
    
    Finally, the useAuth function is a hook that can be used to access the authentication context from other components. It returns the authentication context that was defined in the AuthProvider component.

---
+++ src/hooks/auth.jsx
@@ -0,0 +1,108 @@
+import { createContext, useContext, useState, useEffect } from 'react'
+
+import { api } from '../services/api'
+
+export const AuthContext = createContext({})
+
+function AuthProvider({ children }) {
+    const [data, setData] = useState({})
+    const [loading, setLoading] = useState(false)
+
+    async function signIn({ email, password }) {
+        try {
+            setLoading(true)
+
+            const response = await api.post('/sessions', { email, password })
+            const { user, token } = response.data
+
+            localStorage.setItem('@foodExplorer:user', JSON.stringify(user))
+            localStorage.setItem('@foodExplore:token', token)
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+            setData({ user, token })
+            setLoading(false)
+        } catch (error) {
+            if (error.response) {
+                alert(error.response.data.message)
+            } else {
+                alert('Não foi possível entrar.')
+            }
+
+            setLoading(false)
+        }
+    }
+
+    function signOut() {
+        localStorage.removeItem('@foodExplorer:user')
+        localStorage.removeItem('@foodExplorer:token')
+
+        setData({})
+    }
+
+    async function updateProfile({ user, avatarFile }) {
+        try {
+            if (avatarFile) {
+                setLoading(true)
+
+                const fileUpdateForm = new FormData()
+                fileUpdateForm.append('avatar', avatarFile)
+
+                const response = await api.patch('users/avatar', fileUpdateForm)
+
+                user.avatar = response.data.avatar
+            }
+
+            await api.put('/users', user)
+
+            localStorage.setItem({ user, token: data.token })
+
+            alert('Perfil atualizado com sucesso!')
+
+            setLoading(false)
+        } catch (error) {
+            if (error.response) {
+                alert(error.response.data.message)
+            } else {
+                alert('Não foi possível atualizar o perfil.')
+            }
+
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem('@foodExplorer:token')
+        const user = localStorage.getItem('@foodExplorer:user')
+
+        if (token && user) {
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            })
+        }
+    }, [])
+
+    return (
+        <AuthContext.Provider value={{
+            signIn,
+            signOut,
+            loading,
+            setLoading,
+            updateProfile,
+            user: data.user
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+function useAuth() {
+    const context = useContext(AuthContext)
+
+    return context
+}
+
+export { AuthProvider, useAuth }
