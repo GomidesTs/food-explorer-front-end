cd6c7e249ccb7316ff72f6eead32ba38bc080d8e
GomidesTs <gomidestulio5@gmail.com>
2023-04-05 16:39:23 -0300

    :sparkles: feat: choice of routes
    This update added user authentication system integration to the index.jsx file located in the routes directory. With this implementation, it was possible to define that, if a user is authenticated, the Home page will be rendered for him. Otherwise, the Login page will be displayed, offering the option of redirecting to the user registration page, if the user is not yet registered in the application.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 
+import { useAuth } from '../hooks/auth'
+
+import { AuthRoutes } from './auth.routes'
 import { AppRoutes } from './app.routes'
 
 export function Routes() {
+    const { user } = useAuth()
+    
     return (
         <BrowserRouter>
-            <AppRoutes />
+            {user ? <AppRoutes /> : <AuthRoutes />}
         </BrowserRouter>
     )
 }
