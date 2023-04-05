fc94a103f37028d0a104d7693f2c1c4c55da5ab5
GomidesTs <gomidestulio5@gmail.com>
2023-04-05 16:45:23 -0300

    :sparkles: feat: profile route
    This update added the profile route to the set of available routes in the authenticated application. This route will allow the user to view the information saved in the application, as well as allow him to update it as he deems necessary.

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
-import { Home } from '../pages/Home/index'
+
+import { Home } from '../pages/Home'
 import { Details } from '../pages/Details'
 import { CreateDish } from '../pages/CreateDish'
+import { Profile } from '../pages/Profile'
 
 export function AppRoutes() {
     return (
@@ -9,6 +11,7 @@ export function AppRoutes() {
             <Route path='/' element={<Home />} />
             <Route path='/details/:id' element={<Details />} />
             <Route path='/createdish' element={<CreateDish />} />
+            <Route path='/profile' element={<Profile />} />
         </Routes>
     )
 }
