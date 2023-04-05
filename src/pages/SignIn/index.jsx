0770a7b945d5a30748fa2e6705d60bad920da964
GomidesTs <gomidestulio5@gmail.com>
2023-04-05 17:06:07 -0300

    :recycle: refactor: behaviors and icons
    This update defined that the application's forms lost the autoComplete functionality. This change was implemented to prevent forms layout from breaking. Also, the 'RiEyeCloseLine' icon has been replaced with 'RiEyeOffLine' to represent the closed eye when showing the hidden password. This change was made to better serve the purpose of hiding the password, providing greater visual clarity for the user. We believe this update will improve the user experience when using the application, making the login process easier and more intuitive.

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { RiEyeCloseLine, RiEyeLine } from 'react-icons/ri'
+import { RiEyeOffLine, RiEyeLine } from 'react-icons/ri'
 
 import { useAuth } from '../../hooks/auth'
 
@@ -15,6 +15,7 @@ export function SignIn() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [showPassword, setShowPassword] = useState(false)
+    
     const { signIn, loading } = useAuth()
 
     function handleSignIn() {
@@ -35,7 +36,7 @@ export function SignIn() {
         <Container>
             <img src={LogoFoodExplore} alt='Logo do restaurante fictício Food Explore, este logo tem um losango na cor azul-escuro e ao lago escrito Food Explore na cor branca.' />
 
-            <Form>
+            <Form autoComplete='off'>
                 <fieldset>
 
                     <legend>Faça login</legend>
@@ -64,7 +65,7 @@ export function SignIn() {
                             />
 
                             <span onClick={handleTogglePassword}>
-                                {showPassword ? <RiEyeCloseLine size={22} />: <RiEyeLine size={22} /> }
+                                {showPassword ? <RiEyeOffLine size={22} />: <RiEyeLine size={22} /> }
                             </span>
                         </div>
                     </div>
