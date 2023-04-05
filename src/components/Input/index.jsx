ef801dee4f0c9328de31436f134af1f8f4a1528e
GomidesTs <gomidestulio5@gmail.com>
2023-04-05 18:54:15 -0300

    :sparkles: feat: icons
    This update added the ability to include icons in its container to the Input component to improve the visual appearance of the application. This will allow developers to further customize the forms and add relevant icons to provide more clarity and ease of use for the user. Additionally, this functionality can improve usability on mobile devices, where screen space is limited and the inclusion of icons can help highlight application functionality.

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -3,9 +3,9 @@ import { Container } from './styles'
 export function Input({ icon: Icon, ...rest }) {
     return (
         <Container>
-            {Icon && <Icon size={20} />}
-            
-            <input {...rest} />
+                {Icon && <Icon size={20} />}
+
+                <input {...rest} />
         </Container>
     )
 }
