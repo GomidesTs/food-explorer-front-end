82e143d156d927a936b71ac266717939c8bd9a01
GomidesTs <gomidestulio5@gmail.com>
2023-04-05 18:29:45 -0300

    :lipstick: feat: style change
    This update added new styling to the Input component, which included using display flex to position icons left and centered within the component. In addition, the background of the input was removed so that the container has the same color throughout its length, creating a more cohesive layout.

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,18 +1,27 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+   display: flex;
+   align-items: center;
+   gap: .8rem;
+   height: 4.8rem;
+
+   margin-bottom: 2rem;
+
+   background-color: ${({ theme }) => theme.COLORS.DARK_200};
+   
+   border-radius: .8rem;
+   
+   overflow: hidden;
+   
+   padding: .8rem;
+
    input {
-        width: 100%;
-        height: 4.8rem;
-
-        padding: 1.6rem 1.4rem;
-        margin-bottom: 3.2rem;
-
-        background-color: ${({theme})=> theme.COLORS.DARK_200};
-
-        border: none;
-        border-radius: .8rem;
-
-        color: ${({theme})=> theme.COLORS.LIGHT_400};
+      width: 100%;
+      
+      background: none;
+      border: none;
+      
+      color: ${({ theme }) => theme.COLORS.LIGHT_400};
    }
-`
+   `
