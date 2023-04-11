3f5e17ed3a287efe1d7e809415a018f8c7337cb9
GomidesTs <gomidestulio5@gmail.com>
2023-04-11 09:16:36 -0300

    :sparkles: feat: conditional rendering of icons
    This update implements conditional rendering for regular users and administrators. This allows the application to display a visual demonstration of the user who is accessing it, as well as the specific functions available for each type of user. In addition, there was an update to the icon package, changing from 'react-icons/fi' to 'react-icons/ri', thus ensuring consistency and uniform design in the application.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
-import { FiSearch, FiUser, FiLogOut } from 'react-icons/fi'
+import { RiSearchLine, RiLoginBoxLine, RiDraftLine, RiAddLine } from 'react-icons/ri'
 
 import { api } from '../../services/api'
 import { useAuth } from '../../hooks/auth'
 
-import { Container, Content, Search, Profile, Logout } from './styles'
+import { Container, Content, Search, Profile, Dish, Logout } from './styles'
+
+import { Button } from '../Button';
 
 import LogoFoodExplore from '../../assets/LogoFoodExplore.svg'
 import avatarPlaceholder from '../../assets/avatarPlaceholder.svg'
@@ -12,7 +14,7 @@ import avatarPlaceholder from '../../assets/avatarPlaceholder.svg'
 
 export function Header() {
     const { signOut, user } = useAuth()
-    
+
     const avatarURL = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
     return (
@@ -20,12 +22,19 @@ export function Header() {
             <Content>
                 <Link to='/'>
                     <img src={LogoFoodExplore} alt='Logo do restaurante fictício Food Explore, este logo tem um losango na cor azul-escuro e ao lago escrito Food Explore na cor branca.' />
+                    {
+                        user.isAdmin
+                            ?
+                            <p>admin</p>
+                            :
+                            <p>Boas compras</p>
+                    }
                 </Link>
             </Content>
 
             <Search>
                 <label htmlFor='search'>
-                    <FiSearch size={24} />
+                    <RiSearchLine size={24} />
 
                     <input
                         type='text'
@@ -42,8 +51,24 @@ export function Header() {
                 </Link>
             </Profile>
 
+            <Dish>
+                {
+                    user.isAdmin
+                        ?
+                        <Button
+                        title='Novo prato'
+                        icon={RiAddLine}
+                        />
+                        :
+                        <Button
+                            title={`Pedidos (0)`}
+                            icon={RiDraftLine}
+                        />
+                }
+            </Dish>
+
             <Logout to='/' onClick={signOut}>
-                <FiLogOut size={24} />
+                <RiLoginBoxLine size={24} />
             </Logout>
 
         </Container>
