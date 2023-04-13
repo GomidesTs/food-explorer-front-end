9651c0d31138212a7bddc387bc4a2dbcddfb7a01
GomidesTs <gomidestulio5@gmail.com>
2023-04-13 12:29:57 -0300

    :bricks: ci: global settings
    In this project update, several changes were made to the structure of the code added to the main.jsx file.
    
    The following elements were inserted:
    
    - ThemeProvider: allow the insertion of colors defined in the theme in any project's styling file. This approach helps maintain visual consistency throughout the application and makes the code easier to maintain.
    
    - GlobalStyle: created to define default styles that will be exported throughout the application and used for repetitive styling that will be applied in a single place. This approach helps reduce code repetition and makes code maintenance easier.
    
    - AuthProvider: inserted to provide user data in the application's routes. This functionality is essential for the operation of several parts of the application, such as user authentication and access permissions management.
    
    - Routes: Added to allow browsing via routes. This element is fundamental for the construction of modern web applications and allows the creation of dynamic and interactive interfaces.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+
+import theme from './styles/theme'
+import GlobalStyle from './styles/global'
+
+import { Routes } from './routes'
+import { AuthProvider } from './hooks/auth'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
