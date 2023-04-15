04324302cf19a054bc645af2dda3cafc80b5f9a2
GomidesTs <gomidestulio5@gmail.com>
2023-04-15 14:25:09 -0300

    :sparkles: feat: routes
    This update added a new route in the application that allows editing dishes. This functionality will only be available to users with administrator authorization. With this new route, it will be possible to change important information about the dish, such as the photo, name, category, ingredients, price and details. This way, administrators will be able to make adjustments and updates to the list of dishes offered by the application.

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { Details } from '../pages/Details'
 import { CreateDish } from '../pages/CreateDish'
+import { EditDish } from '../pages/EditDish'
 import { Profile } from '../pages/Profile'
 
 export function AppRoutes() {
@@ -11,6 +12,7 @@ export function AppRoutes() {
             <Route path='/' element={<Home />} />
             <Route path='/details/:id' element={<Details />} />
             <Route path='/createdish' element={<CreateDish />} />
+            <Route path='/editdish/:id' element={<EditDish />} />
             <Route path='/profile' element={<Profile />} />
         </Routes>
     )
