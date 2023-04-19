330b4d967f71a2cd4d59a55c63fd02df3b842554
GomidesTs <gomidestulio5@gmail.com>
2023-04-19 17:33:31 -0300

    :sparkles: feat: favorites page
    This update adds the 'Favorites' page to the app. This page was created based on the structure of the 'Home' page and the styling was modified to suit the new functionality. The new page displays a list of dishes marked as favorites by the user, using the 'fetchDishes' function to fetch these products from the database and render them on screen.

---
+++ src/pages/Favorites/index.jsx
@@ -0,0 +1,136 @@
+import { useState, useEffect } from 'react'
+import { SwiperSlide } from 'swiper/react'
+
+import { Container, Content, Banner, Dishes } from './styles'
+
+import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+import { Footer } from './../../components/Footer'
+import { Carousel } from './../../components/Carousel'
+import { Card } from './../../components/Card'
+
+import { api } from '../../services/api'
+
+import Food from '../../assets/foods.png'
+
+
+export function Favorites() {
+    const settings = {
+        navigation: true,
+        loop: true,
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 40
+            }
+        }
+    }
+
+    const [dishes, setDishes] = useState([])
+    const [search, setSearch] = useState('')
+
+    useEffect(() => {
+        async function fetchDishes() {
+            const response = await api.get(`/favorites?title=${search}`)
+            setDishes(response.data)
+        }
+
+        fetchDishes()
+    }, [search,dishes])
+
+    return (
+        <Container>
+            <Header search={setSearch} />
+
+            <Content>
+                <Banner>
+                    <img src={Food} alt='Biscoitos recheados e coloridos com frutinhas vermelhas e folhas caindo ao seu redor.' />
+
+                    <div className='card'>
+                        <div className='title'>
+                            <h1>Sabores inigualáveis</h1>
+
+                            <span>Sinta o cuidado do preparo com ingredientes selecionados</span>
+                        </div>
+                    </div>
+                </Banner>
+
+                <Dishes>
+                    <Section
+                        title='Refeições'
+                    />
+                    {
+                        dishes.filter(dish => dish.category == 'snack').length > 0 &&
+                        <Carousel settings={settings}>
+                            {
+                                dishes.filter(dish => dish.category == 'snack').map(dish => (
+                                    <SwiperSlide
+                                        key={String(dish.id)}
+                                    >
+                                        <Card
+                                            data={dish}
+                                        />
+                                    </SwiperSlide>
+                                ))
+                            }
+
+                        </Carousel>
+                    }
+
+                    <Section
+                        title='Sobremesas'
+                    />
+                    {
+                        dishes.filter(dish => dish.category == 'dessert').length > 0 &&
+                        <Carousel settings={settings}>
+                            {
+                                dishes.filter(dish => dish.category == 'dessert').map(dish => (
+                                    <SwiperSlide
+                                        key={String(dish.id)}
+                                    >
+                                        <Card
+                                            data={dish}
+                                        />
+                                    </SwiperSlide>
+                                ))
+                            }
+
+                        </Carousel>
+                    }
+
+                    <Section
+                        title='Bebidas'
+                    />
+                    {
+                        dishes.filter(dish => dish.category == 'drink').length > 0 &&
+                        <Carousel settings={settings}>
+                            {
+                                dishes.filter(dish => dish.category == 'drink').map(dish => (
+                                    <SwiperSlide
+                                        key={String(dish.id)}
+                                    >
+                                        <Card
+                                            data={dish}
+                                        />
+                                    </SwiperSlide>
+                                ))
+                            }
+
+                        </Carousel>
+                    }
+                </Dishes>
+
+                <Footer />
+            </Content>
+
+        </Container>
+    )
+}
---
+++ src/pages/Favorites/styles.js
@@ -0,0 +1,86 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    max-width: 100%;
+    height: 100vh;
+
+    display: grid;
+    grid-template-rows: 10rem auto;
+    grid-template-areas: 
+    'header'
+    'content';
+`
+
+export const Content = styled.main`
+    grid-area: content;
+
+    overflow-y: auto;
+
+    padding-top: 5rem;
+`
+
+export const Banner = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    text-align: center;
+
+    .card {
+        background: ${({ theme }) => theme.COLORS.GRADIENTS_100};
+
+        border-radius: 2rem;
+
+        position: relative;
+
+        padding: 2rem;
+
+        .title {
+            padding: 1rem;
+
+            h1 {
+                line-height: 140%;
+                font-size: clamp(2rem, 4vw, 4rem);
+                font-weight: 500;
+
+                padding-bottom: .8rem;
+            }
+
+            span {
+                font-size: clamp(1.4rem, 2.5vw, 2rem);
+            }
+        }
+    }
+
+    img {
+        width: 50%;
+        margin-bottom: -2.7rem;
+        z-index: -1;
+    }
+
+    @media only screen and (min-width: 1000px) {
+        flex-direction: row;
+        
+        .card {
+            .title {
+                width: 100%;
+                height: 26rem;
+
+                padding: 8.7rem 4.6rem 0 0;
+
+            }
+        }
+
+        img {
+            width: 30%;
+        }
+    }
+`
+
+export const Dishes = styled.div`
+    max-width: 120rem;
+    
+    padding: 6.4rem 3rem;
+    margin: 0 auto;
+`
