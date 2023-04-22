11564e7a65e7ad54d2a7d39bab069ed32e2f798f
GomidesTs <gomidestulio5@gmail.com>
2023-04-21 23:59:14 -0300

    :lipstick: renovated style
    This update focused on restyled the bookmarks page, making it visually different from the Home page, with a completely different structure. This way, users will be able to immediately identify that they are on the favorites page and not the Home page.

--- src/pages/Favorites/styles.js
+++ src/pages/Favorites/styles.js
@@ -8,79 +8,62 @@ export const Container = styled.div`
     grid-template-rows: 10rem auto;
     grid-template-areas: 
     'header'
-    'content';
+    'content'
+    'footer';
 `
 
 export const Content = styled.main`
     grid-area: content;
 
     overflow-y: auto;
-
-    padding-top: 5rem;
-`
-
-export const Banner = styled.section`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    text-align: center;
-
-    .card {
-        background: ${({ theme }) => theme.COLORS.GRADIENTS_100};
-
-        border-radius: 2rem;
-
-        position: relative;
-
-        padding: 2rem;
-
-        .title {
-            padding: 1rem;
-
-            h1 {
-                line-height: 140%;
-                font-size: clamp(2rem, 4vw, 4rem);
-                font-weight: 500;
-
-                padding-bottom: .8rem;
-            }
-
-            span {
-                font-size: clamp(1.4rem, 2.5vw, 2rem);
-            }
-        }
-    }
-
-    img {
-        width: 50%;
-        margin-bottom: -2.7rem;
-        z-index: -1;
-    }
-
-    @media only screen and (min-width: 1000px) {
-        flex-direction: row;
-        
-        .card {
-            .title {
-                width: 100%;
-                height: 26rem;
-
-                padding: 8.7rem 4.6rem 0 0;
-
-            }
-        }
-
-        img {
-            width: 30%;
-        }
-    }
 `
 
 export const Dishes = styled.div`
     max-width: 120rem;
     
-    padding: 6.4rem 3rem;
     margin: 0 auto;
+    `
+
+export const Options = styled.div`
+    max-width: 100rem;
+    margin: 0 auto;
+
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-self: center;
+    justify-content: space-around;
+    gap: 3.6rem;
+
+    @media only screen and (min-width: 600px) {
+        flex-direction: row;
+        justify-content: start;
+    }
+`
+
+export const Favorite = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1.6rem;
+
+    width: 30rem;
+
+    img {
+        width: clamp(5rem, 15vw, 10rem);
+        height: clamp(5rem, 15vw, 10rem);
+        object-fit: cover;
+
+        border-radius: 50%;
+    }
+
+    h3 {
+        font-size: 1.8rem;
+    }
+
+    button {
+        background: none;
+        border: none;
+
+        color: ${({theme})=> theme.COLORS.TOMATO_100}
+    }
 `
