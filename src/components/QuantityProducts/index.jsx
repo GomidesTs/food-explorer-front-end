9a591fd93dc7b0e5ab4388585fb61f5d4d93ac94
GomidesTs <gomidestulio5@gmail.com>
2023-04-22 22:03:37 -0300

    :recycle: refator:  quantity of products
    In this update, there was a reformulation in the way the quantity of products is accounted for in the project. Now each entry is calculated and interpreted independently, with the increment, decrement, and include buttons encapsulated in a single component. Also, the add button inserts the price at the end of your text, and if price is provided for the component, it calculates the new value to add based on the original price and quantity supplied by the user. With these changes, it is expected to improve the accuracy and functionality of counting products in the project.

--- src/components/QuantityProducts/index.jsx
+++ src/components/QuantityProducts/index.jsx
@@ -4,39 +4,68 @@ import { RiAddFill, RiSubtractFill } from 'react-icons/ri'
 import { Container } from './styles'
 
 import { ButtonText } from '../ButtonText'
+import { Button } from '../Button'
 
-export function QuantityProducts() {
+export function QuantityProducts({ dish_id, price }) {
     const [quantity, setQuantity] = useState(0)
+    const [priceProduct, setPriceProduct] = useState(price)
 
-    function increase() {
-        if (quantity > 19) {
+    function increase(amount) {
+        if (amount > 20) {
             alert('Erro: A quantidade máxima é de 20 unidades')
             return;
         }
-        setQuantity(count => count + 1);
+
+        setQuantity(amount)
+        setPriceProduct(price * amount)
     }
 
-    function decrease() {
+    function decrease(amount) {
+        if (quantity < 2) {
+            alert('Erro: A quantidade mínima é 1 unidade')
+            return
+        }
+
+        setQuantity(amount)
+        setPriceProduct(price * amount)
+    }
+
+    function handleAddDish() {
         if (quantity < 1) {
             alert('Erro: A quantidade mínima é 1 unidade')
             return
         }
-        setQuantity(count => count - 1)
+        console.log(dish_id);
     }
 
     return (
         <Container>
             <ButtonText className='decrease'
                 icon={RiSubtractFill}
-                onClick={decrease}
+                onClick={() => decrease(quantity - 1)}
             />
 
             <span>{quantity.toString().padStart(2, '0')}</span>
 
             <ButtonText className='increase'
                 icon={RiAddFill}
-                onClick={increase}
+                onClick={() => increase(quantity + 1)}
             />
+
+            <div>
+                <Button
+                    title={`
+                            Incluir
+                            ${price
+                            ?
+                            `∙ ${priceProduct}`
+                            :
+                            ''
+                        }
+                    `}
+                    onClick={handleAddDish}
+                />
+            </div>
         </Container>
     )
 }
