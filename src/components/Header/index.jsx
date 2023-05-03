f4c75fb398bb8ce558fcc6b0abe3e6ac833fa82f
GomidesTs <gomidestulio5@gmail.com>
2023-05-03 15:47:40 -0300

    :sparkles: feat: header
    This update was made in the Header component of the project, in order to adapt to the new pages available. Therefore, the component has been reworked to meet the needs of these pages.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
+import { useState, useEffect } from 'react'
+
 import { Link } from 'react-router-dom'
-import { RiAlignJustify, RiSearchLine, RiLoginBoxLine, RiDraftLine, RiAddLine, RiHeartLine } from 'react-icons/ri'
+import { RiAlignJustify, RiSearchLine, RiCloseLine, RiLoginBoxLine, RiDraftLine, RiAddLine, RiHeartFill } from 'react-icons/ri'
 
 import { api } from '../../services/api'
 import { useAuth } from '../../hooks/auth'
 
-import { Container, Content, Search, Profile, Dish, Logout } from './styles'
+import { Container } from './styles'
 
 import { Button } from '../Button';
 
@@ -14,68 +16,172 @@ import avatarPlaceholder from '../../assets/avatarPlaceholder.svg'
 
 export function Header({ search }) {
     const { signOut, user } = useAuth()
+    const [cart, setCart] = useState(0)
+
     const avatarURL = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+
+    function handleMenu() {
+        document.getElementById('menu').classList.toggle('active')
+        document.querySelector('.open').classList.toggle('hide')
+        document.querySelector('.close').classList.toggle('show')
+    }
+
+    useEffect(() => {
+        async function fetchCarts() {
+            const response = await api.get('/carts')
+            setCart(response.data)
+        }
+
+        fetchCarts()
+    }, [cart])
 
     return (
         <Container>
-            <Content>
-                <Link to='/'>
-                    <img src={LogoFoodExplore} alt='Logo do restaurante fictício Food Explore, este logo tem um losango na cor azul-escuro e ao lago escrito Food Explore na cor branca.' />
+            <menu>
+                <div className='mobile'>
+                    <div className='icon'>
+                        <RiAlignJustify
+                            size={24}
+                            onClick={handleMenu}
+                            className='open'
+                        />
+
+                        <RiCloseLine
+                            size={26}
+                            onClick={handleMenu}
+                            className='close'
+                        />
+                    </div>
+
+                    <nav id='menu' className='menu'>
+                        <div className='search'>
+                            <label htmlFor='search'>
+                                <RiSearchLine size={24} />
+
+                                <input
+                                    type='search'
+                                    name='search'
+                                    id='search'
+                                    placeholder='Busque por pratos'
+                                    autocomplete='off'
+                                    onChange={e => { search(e.target.value) }}
+                                />
+                            </label>
+                        </div>
+                        <div className='links'>
+                            {
+                                user.isAdmin
+                                    ?
+                                    <div className='adim'>
+                                        <Link to='/orders'>Histórico de pedidos</Link>
+                                        <Link to='/createdish'>Novo prato</Link>
+                                    </div>
+                                    :
+                                    <div className='user'>
+                                        <Link to='/favorites'>Meus favoritos</Link>
+                                        <Link to='/orders'>Histórico de pedidos</Link>
+                                    </div>
+                            }
+
+                            <Link to='/profile'>Área do usuário</Link>
+                            <Link to='/' onClick={signOut}>Sair</Link>
+                        </div>
+                    </nav>
+
+                    <Link to='/' className='logo'>
+                        <img src={LogoFoodExplore} alt='Logo do restaurante fictício Food Explore, este logo tem um losango na cor azul-escuro e ao lago escrito Food Explore na cor branca.' />
+                        {
+                            user.isAdmin
+                                ?
+                                <p>admin</p>
+                                :
+                                <p className='welcome'>Bon appétit</p>
+                        }
+                    </Link>
+
+                    <span className='createDish'>
+                        {
+                            !user.isAdmin &&
+                            <Link to={'/carts'}>
+                                <div className='mobileDish'>
+                                    <RiDraftLine size={26} />
+                                    <p>{cart.length}</p>
+                                </div>
+                            </Link>
+                        }
+                    </span>
+                </div>
+
+                <div className='desktop'>
+                    <Link to='/' className='logo'>
+                        <img src={LogoFoodExplore} alt='Logo do restaurante fictício Food Explore, este logo tem um losango na cor azul-escuro e ao lago escrito Food Explore na cor branca.' />
+                        {
+                            user.isAdmin
+                                ?
+                                <p>admin</p>
+                                :
+                                <p>Bon appétit</p>
+                        }
+                    </Link>
+
+                    <div className='search'>
+                        <label htmlFor='search'>
+                            <RiSearchLine size={24} />
+
+                            <input
+                                type='search'
+                                name='search'
+                                id='search'
+                                placeholder='Busque por pratos'
+                                autocomplete='off'
+                                onChange={e => { search(e.target.value) }}
+                            />
+                        </label>
+                    </div>
+
+                    {
+                        !user.isAdmin &&
+                        <Link to={'/favorites'}>
+                            <div className='mobileDish'>
+                                <RiHeartFill size={26} />
+                            </div>
+                        </Link>
+                    }
+
+                    <Link to='/orders'>Histórico de pedidos</Link>
+
+                    <Link to={'/profile'} className='profile'>
+                        <img src={avatarURL} alt="" />
+                    </Link>
+
                     {
                         user.isAdmin
                             ?
-                            <p>admin</p>
+                            <Link to={'/createdish'}>
+                                <Button
+                                    title='Novo prato'
+                                    icon={RiAddLine}
+                                />
+                            </Link>
                             :
-                            <p>Bon appétit</p>
+                            <Link to={'/carts'}>
+                                <Button
+                                    title={
+                                        cart
+                                            ?
+                                            `Pedidos (${cart.length})`
+                                            :
+                                            `Pedidos (0)`
+                                    }
+                                    icon={RiDraftLine}
+                                />
+                            </Link>
                     }
-                </Link>
-            </Content>
 
-            <Search>
-                <label htmlFor='search'>
-                    <RiSearchLine size={24} />
+                    <Link to='/' onClick={signOut}><RiLoginBoxLine size={24} /></Link>
 
-                    <input
-                        type='search'
-                        name='search'
-                        id='search'
-                        placeholder='Busque por pratos ou ingredientes'
-                        autocomplete='off'
-                        onChange={e => { search(e.target.value) }}
-                    />
-                </label>
-            </Search>
-
-            <Profile>
-                <Link to={'/profile'}>
-                    <img src={avatarURL} alt="" />
-                </Link>
-            </Profile>
-
-            <Dish>
-                {
-                    user.isAdmin
-                        ?
-                        <Link to={'/createdish'}>
-                            <Button
-                                title='Novo prato'
-                                icon={RiAddLine}
-                            />
-                        </Link>
-                        :
-                        <Link to={'/'}>
-                            <Button
-                                title={`Pedidos (0)`}
-                                icon={RiDraftLine}
-                            />
-                        </Link>
-                }
-            </Dish>
-
-            <Logout to='/' onClick={signOut}>
-                <RiLoginBoxLine size={24} />
-            </Logout>
-
+                </div>
+            </menu>
         </Container>
     )
 }
